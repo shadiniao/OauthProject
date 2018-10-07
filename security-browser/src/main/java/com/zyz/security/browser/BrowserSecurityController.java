9f0c84fa68d82f0372a769c9d37c4957871e6246
zhangyizhi <157876195@qq.com>
2018-10-07 23:18:11 +0800

    v24 社交登录第一步

--- security-browser/src/main/java/com/zyz/security/browser/BrowserSecurityController.java
+++ security-browser/src/main/java/com/zyz/security/browser/BrowserSecurityController.java
@@ -1,6 +1,7 @@
 package com.zyz.security.browser;
 
 import com.zyz.security.browser.support.SimpleResponse;
+import com.zyz.security.browser.support.SocialUserInfo;
 import com.zyz.security.core.properties.SecurityConstants;
 import com.zyz.security.core.properties.SecurityProperties;
 
@@ -14,9 +15,13 @@ import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.web.ProviderSignInUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
 
 import java.io.IOException;
 
@@ -30,31 +35,46 @@ import javax.servlet.http.HttpServletResponse;
  */
 @RestController
 public class BrowserSecurityController {
-    private Logger logger = LoggerFactory.getLogger(getClass());
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private RequestCache requestCache = new HttpSessionRequestCache();
+	private RequestCache requestCache = new HttpSessionRequestCache();
 
-    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
-    @Autowired
-    private SecurityProperties securityProperties;
+	@Autowired
+	private SecurityProperties securityProperties;
 
-    /**
-     * 需要身份认证时跳转到此
-     */
-    @RequestMapping(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
-    @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
-    public SimpleResponse requireAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        SavedRequest savedRequest = requestCache.getRequest(request, response);
+	@Autowired
+	private ProviderSignInUtils providerSignInUtils;
 
-        if (savedRequest != null) {
-            String targetUrl = savedRequest.getRedirectUrl();
-            logger.info("引发跳转的请求是:" + targetUrl);
-            if (StringUtils.endsWithIgnoreCase(targetUrl, ".html")) {
-                redirectStrategy.sendRedirect(request, response, securityProperties.getBrowser().getLoginPage());
-            }
-        }
+	/**
+	 * 需要身份认证时跳转到此
+	 */
+	@RequestMapping(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+	@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+	public SimpleResponse requireAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		SavedRequest savedRequest = requestCache.getRequest(request, response);
 
-        return new SimpleResponse("访问的服务需要身份认证,请转到登录页");
-    }
+		if (savedRequest != null) {
+			String targetUrl = savedRequest.getRedirectUrl();
+			logger.info("引发跳转的请求是:" + targetUrl);
+			if (StringUtils.endsWithIgnoreCase(targetUrl, ".html")) {
+				redirectStrategy.sendRedirect(request, response, securityProperties.getBrowser().getLoginPage());
+			}
+		}
+
+		return new SimpleResponse("访问的服务需要身份认证,请转到登录页");
+	}
+
+	@GetMapping("/social/user")
+	public SocialUserInfo getSocialUserInfo(HttpServletRequest request) {
+		SocialUserInfo userInfo = new SocialUserInfo();
+		Connection<?> connection = providerSignInUtils.getConnectionFromSession(new ServletWebRequest(request));
+		userInfo.setProviderId(connection.getKey().getProviderId());
+		userInfo.setProviderUserId(connection.getKey().getProviderUserId());
+		userInfo.setNickname(connection.getDisplayName());
+		userInfo.setHeadimg(connection.getImageUrl());
+
+		return userInfo;
+	}
 }
---
+++ security-browser/src/main/java/com/zyz/security/browser/support/SocialUserInfo.java
@@ -0,0 +1,48 @@
+package com.zyz.security.browser.support;
+
+/**
+ * 2018/9/22.
+ *
+ * @author zhangyizhi
+ */
+public class SocialUserInfo {
+	private String providerId;
+
+	private String providerUserId;
+
+	private String nickname;
+
+	private String headimg;
+
+	public String getProviderId() {
+		return providerId;
+	}
+
+	public void setProviderId(String providerId) {
+		this.providerId = providerId;
+	}
+
+	public String getProviderUserId() {
+		return providerUserId;
+	}
+
+	public void setProviderUserId(String providerUserId) {
+		this.providerUserId = providerUserId;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getHeadimg() {
+		return headimg;
+	}
+
+	public void setHeadimg(String headimg) {
+		this.headimg = headimg;
+	}
+}
